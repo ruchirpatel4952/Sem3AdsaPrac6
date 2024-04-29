4e975af0c59e99b6d34cb864b172e0635e1e871f
Ruchir <ruchirpatel2000@gmail.com>
2024-04-29 12:00:31 +0930

    fourth commit

---
+++ BigNumCalc copy.h
@@ -0,0 +1,15 @@
+#ifndef BIGNUMCALC_H
+#define BIGNUMCALC_H
+
+#include <list>
+#include <string>
+
+class BigNumCalc {
+public:
+    std::list<int> buildBigNum(const std::string& numString);
+    std::list<int> add(const std::list<int>& num1, const std::list<int>& num2);
+    std::list<int> sub(const std::list<int>& num1, const std::list<int>& num2);
+    std::list<int> mul(const std::list<int>& num1, const std::list<int>& num2);
+};
+
+#endif // BIGNUMCALC_H
