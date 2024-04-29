594a6e174bee5a8618f6aba47459b6f1529bc06b
Ruchir <ruchirpatel2000@gmail.com>
2024-04-29 12:23:57 +0930

    fourth commit

--- Node.h
+++ Node.h
@@ -6,8 +6,6 @@ class Node {
     Node* link;
     friend class LinkedList; // Allows LinkedList full access to Node's internals
 
-public:
-    Node(int data, Node* next = nullptr) : data(data), link(next) {}
 };
 
 #endif // NODE_H
